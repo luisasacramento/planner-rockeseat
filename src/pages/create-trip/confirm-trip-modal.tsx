58da98a241a59a8853748059ed6d95a50dd78a8f
Luisa Santos <luisacristina.sn@icloud.com>
2024-07-11 00:58:47 -0300

    componentes globais

--- src/pages/create-trip/confirm-trip-modal.tsx
+++ src/pages/create-trip/confirm-trip-modal.tsx
@@ -1,5 +1,6 @@
 import { Mail, User, X } from "lucide-react";
 import { FormEvent } from "react";
+import { Button } from "../../components/button";
 
 interface ConfirmTripModalProps {
     closeConfirmTripModal: () => void
@@ -45,10 +46,9 @@ export function ConfirmTripModal({
                             className="bg-transparent text-md placeholder-zinc-400 outline-none flex-1" />
                     </div>
 
-                    <button type='submit' className='w-full justify-center bg-lime-300 text-lime-950 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-lime-400'>
+                    <Button type="submit" variant="primary" size="full">
                         Confirmar criação de viagem
-
-                    </button>
+                    </Button>
                 </form>
             </div>
         </div>
--- src/pages/trip-details/create-activity-modal.tsx
+++ src/pages/trip-details/create-activity-modal.tsx
@@ -1,4 +1,5 @@
 import { Calendar, Tag, X } from "lucide-react"
+import { Button } from "../../components/button"
 
 interface CreateActivityModalProps {
     closeCreatedActivityyModal: () => void
@@ -46,10 +47,10 @@ export function CreateActivityModal({
 
                     </div>
 
-                    <button type='submit' className='w-full justify-center bg-lime-300 text-lime-950 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-lime-400'>
+                    <Button variant="primary" size="full">
                         Salvar atividade
 
-                    </button>
+                    </Button>
                 </form>
             </div>
         </div>
