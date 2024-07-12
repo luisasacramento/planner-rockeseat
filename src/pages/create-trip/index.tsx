8cfd21f1faf006cce21704980dac8ef941778c30
Luisa Santos <luisacristina.sn@icloud.com>
2024-07-12 18:45:32 -0300

    conexão backend

--- src/pages/create-trip/index.tsx
+++ src/pages/create-trip/index.tsx
@@ -5,6 +5,7 @@ import { ConfirmTripModal } from './confirm-trip-modal';
 import { DestinationAndDateStep } from './steps/destination-and-date-step';
 import { InviteGuestsStep } from './steps/invite-guests-steps';
 import { DateRange } from 'react-day-picker';
+import { api } from '../../lib/axios';
 
 export function CreateTripPage() {
 
@@ -74,12 +75,46 @@ export function CreateTripPage() {
     }
 
 
-    function createTrip(event: FormEvent<HTMLFormElement>) {
+    async function createTrip(event: FormEvent<HTMLFormElement>) {
         event.preventDefault();
 
+        console.log(destination)
+        console.log(eventStartAndEndDates)
+        console.log(emailsToInvite)
+        console.log(ownerName)
+        console.log(ownerEmail)
+
+
+        if(!destination){
+            return
+        }
+
+        if(!eventStartAndEndDates?.from || !eventStartAndEndDates?.to){
+            return
+        }
+
+        if (emailsToInvite.length === 0){
+            return
+        }
+
+        if(!ownerName || !ownerEmail){
+            return
+        }
+
+
+       const response = await api.post('/trips', {
+            destination,
+            "starts_at": eventStartAndEndDates.from,
+            "ends_at": eventStartAndEndDates.to,
+            "emails_to_invite": emailsToInvite,
+            "owner_name": ownerName,
+            "owner_email": ownerEmail
+        })
+
+        const  { tripId }  = response.data
         
 
-        navigate('/trips/123')
+        navigate(`/trips/${tripId}`)
     }
 
     return (
@@ -99,7 +134,7 @@ export function CreateTripPage() {
                         setDestination={setDestination}
                         setEventStartAndEndDates={setEventStartAndEndDates}
                         eventStartAndEndDates={eventStartAndEndDates}
-                        
+
                     />
 
                     {isGuestsInputOpen && (
